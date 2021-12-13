d4a9f31d3fb5dc5cfddb08c7d4faf03fb73b6245
Chiara <chiara.r.3@live.it>
2021-12-13 21:32:09 +0000

    delete ok

--- components/ListadoTodo.vue
+++ components/ListadoTodo.vue
@@ -1,21 +1,27 @@
 <template>
+<body>
  <div id="todolist">
       <div class="header">
          <h1 :title="header">
             {{ header.toLocaleUpperCase() }}
          </h1>  
-         <button v-if="state === 'default'" class="btn btn-primary" @click="changeState('edit')">Add</button>
-         <button v-else class="btn btn-primary" @click="changeState('default')">Delete</button>
+         <button v-if="state === 'default'" class="btn btn-primary" @click="changeState('edit')">Add new task</button>
+         <button v-else class="btn btn-primary" @click="changeState('default')">Nothing more to do</button>
       </div>
       <div v-if="state === 'edit'" class="add-to-do">
-         <input v-model="newTodo" type="text" placeholder="Add to do" @keyup.enter="saveTodo">
-         <button class="btn btn-primary" @click="saveTodo">Save it</button>
+         <input v-model="newTodo" type="text" placeholder="New one" @keyup.enter="saveTodo">
+         <button class="btn btn-primary" @click="saveTodo">Put it in my list</button>
       </div>
           <ul>
-          <li v-for="todo in todos">{{ todo }}</li>
+          <div><li v-for="(todo,index) in todos" 
+           v-bind:key="todo.id"
+           v-bind:title="todo.task">
+            {{ todo.id }}.{{todo.task}}
+            <button @click="deleteTodo">x</button></li></div>
           </ul>
-          <p v-if="todos.length === 0">Hai fatto tutto quello che dovevi</p>
+          <p v-if="todos.length === 0">Good job! Everything done!</p>
    </div>
+   </body>
 </template>
 
 
@@ -24,21 +30,52 @@ export default {
     data(){
         return{
     state: "default",
-    header: "Otra to do list",
+    header: "To do list",
     newTodo: " ",
     todos: [
-      "Comprar",
-      "Estudiar",
-      "Trabajar",
-    ]
+      {
+          id:1, 
+          task: "Comprar"
+      },
+      {
+          id:2,
+          task: "Estudiar"
+      },
+      {
+          id:3,
+          task:"Trabajar"
+      },
+    ],
+    nextTodoId: 4
   }},
   methods:{
     saveTodo: function(){
-      this.todos.push(this.newTodo);
+      this.todos.push({
+          id: this.nextTodoId++,
+          task: this.newTodo
+      });
       this.newTodo= "";
     },
     changeState: function(newState){
       this.state=newState;
       this.newTodo="";
-    }}}
+    },
+    deleteTodo: function(todos,index){
+        this.todos.splice(index,1);
+        } 
+    }}
    </script>  
+
+   <style scoped>
+body {
+    background-color: #ffb6c1;
+    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    text-align: center;
+}
+h1 {
+    color:red;
+}
+ul.div{
+    border:black
+}
+   </style scoped>
